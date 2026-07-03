77a45662f561cced7e0f31900a44b1941d387207
Korey Alashe <o.a.alashe@paintbookco.co.uk>
2026-07-04 00:57:11 +0100

    fix: add helmet security headers to Express server

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import { handleDemo } from "./routes/demo";
 import authRoutes from "./routes/auth";
 import jobsRoutes from "./routes/jobs";
@@ -18,6 +19,7 @@ export function createServer() {
   const app = express();
 
   // Middleware
+  app.use(helmet());
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
